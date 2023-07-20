0c6cd4f594b86456ed449c4dbac835940613a9e2
Ser Filin <ser_fil@rambler.ru>
2023-07-20 17:20:00 +0400

    Написан код программы RunDog

--- intro_lang_prog/csharp/seminar/RunDog/Program.cs
+++ intro_lang_prog/csharp/seminar/RunDog/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача, блок схема которой была рассмотрена на одной из лекций.
+Написать программу, подсчитывающую сколько раз пробежит собака
+между друзьями, идущими навстречу друг другу.
+*/
+
+int distance = 10000, firstFriendSpeed = 1, secondFriendSpeed = 2,
+    dogSpeed = 5, friend = 2, count = 0, time;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance -= (firstFriendSpeed + secondFriendSpeed) * time;
+    count ++;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз.");
