83b0729ec1e4b4800bb5c481c1d83b33d770f567
Bezbashenyyi <yan_agaf@rambler.ru>
2023-07-25 01:32:40 +0400

    Создан файл 2-го семинарского занятия по До диез

---
+++ intro_lang_prog/csharp/seminar/seminar002/Program.cs
@@ -0,0 +1,11 @@
+﻿int MaxNum(int num)
+{
+    int tens = num / 10;
+    int unit = num % 10;
+    if (tens < unit) return unit;
+    else return tens;
+}
+
+int randNumber = new Random().Next(10, 100);
+
+Console.WriteLine($"В числе {randNumber} наибольшая цифра: {MaxNum(randNumber)}");
