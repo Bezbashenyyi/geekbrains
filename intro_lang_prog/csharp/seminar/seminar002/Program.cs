04b63688217e524cff55046ddbc9eb9d9ef4ef12
Bezbashenyyi <yan_agaf@rambler.ru>
2023-07-27 23:02:05 +0400

    Добавлен третий семинар, создана заготовка для домашнего задания к нему

--- intro_lang_prog/csharp/seminar/seminar002/Program.cs
+++ intro_lang_prog/csharp/seminar/seminar002/Program.cs
@@ -1,4 +1,74 @@
-﻿int MaxNum(int num)
+﻿// Семинар 2.
+
+// Теория.
+
+/*
+    13254 / 10 = 1325
+    13254 / 100 = 132
+    13254 / 1000 = 13
+    13254 / 10000 = 1
+
+    13254 % 10 = 4
+    13254 % 100 = 54
+    13254 % 1000 = 254
+    13254 % 10000 = 3254
+*/
+
+// То, что у меня называется подпрограммой или процедурой,
+// в до диез (C#) называется методом.
+
+/*
+// Задача 1. Написать программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+
+
+int CutNumber(int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+
+    int res = hundreds * 10 + units;
+    return res;
+}
+
+int randNumber = new Random().Next(100, 1000);
+
+int newNumber = CutNumber(randNumber);
+Console.WriteLine($"Новой версией числа {randNumber} является число {newNumber}");
+*/
+
+/*
+// Задача 2. Написать программу, которая получает на вход число и проверяет
+// кратно ли оно одновременно a и b.
+
+
+bool IsDivisible(int num, int div1, div2)
+{
+    // if (num % div1 == 0 && num % div2 == 0)
+    //     return true;
+    // else
+    //     return false;
+
+    return num % div1 == 0 && num % div2 == 0
+}
+
+Console.Write("Введите число для проверки: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первый делитель: ");
+int divider1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второй делитель: ");
+int divider2 = Convert.ToInt32(Console.ReadLine());
+
+bool res = IsDivisible(number, divider1, divider2);
+if (res) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+*/
+
+/*
+// Написать программу, которая выводит случайное число из отрезка
+// от 10 до 99 и показывает наибольшую цифру из этого числа
+
+int MaxNum(int num)
 {
     int tens = num / 10;
     int unit = num % 10;
@@ -9,3 +79,9 @@
 int randNumber = new Random().Next(10, 100);
 
 Console.WriteLine($"В числе {randNumber} наибольшая цифра: {MaxNum(randNumber)}");
+*/
+
+/*
+// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+*/
---
+++ intro_lang_prog/csharp/seminar/seminar003/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+// Напишите программу, которая принимает на вход координаты
+// точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти
+// плоскости, в которой находится эта точка.
+
+int WriteWait(string outLine)
+{
+    Console.Write(outLine);
+    int inNumber = Convert.ToInt32(Console.ReadLine());
+    return inNumber;
+}
+
+bool ChekNull(int abs, int ord)
+{return abs == 0 || ord == 0;}
+
+int Quad(int abs, int ord)
+{
+    if (abs > 0 && ord > 0) return 1;
+    if (abs < 0 && ord > 0) return 2;
+    if (abs < 0 && ord < 0) return 3;
+    return 4;
+}
+
+Repeat:
+int x = WriteWait("Введите координату x: ");
+int y = WriteWait("Введите координату y: ");
+
+if (ChekNull(x, y))
+{
+    Console.WriteLine("Некорректный ввод, пожалуйста введите ещё раз.");
+    goto Repeat;
+}
+
+else Console.WriteLine ($"Координаты лежат в {Quad(x, y)} квадранте");
+*/
+
+
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт квадраты чисел от 1 до N.
+
+int WriteWait(string outLine)
+{
+    Console.Write(outLine);
+    int inNumber = Convert.ToInt32(Console.ReadLine());
+    return inNumber;
+}
+
+
+
+
+// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+// Console.WriteLine(Math.Pow(5, 2));
+// Console.WriteLine(Math.Sqrt(25));
+// Console.WriteLine(Math.Round(Math.Sqrt(24), 2));
