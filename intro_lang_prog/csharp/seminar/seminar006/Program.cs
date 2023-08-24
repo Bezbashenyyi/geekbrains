986a9628ac402443171d4ec645e38c46773f2b87
Bezbashenyyi <yan_agaf@rambler.ru>
2023-08-24 04:46:25 +0400

    Добит 6-ой семинар.

--- intro_lang_prog/csharp/seminar/seminar006/Program.cs
+++ intro_lang_prog/csharp/seminar/seminar006/Program.cs
@@ -1,4 +1,63 @@
 ﻿/*
+// Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на
+// последнем и т.д.)
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+void ReverseArray2(int[] array)
+{
+    for (int i = 0, j = array.Length - 1; i < j; i++, j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(length, min, max);
+ShowArray(myArray);
+ReverseArray(myArray);
+ShowArray(myArray);
+ReverseArray2(myArray);
+ShowArray(myArray);
+*/
+
+
+/*
 // Напишите программу, которая принимает на вход три числа и проверяет,
 // может ли существовать треугольник со сторонами такой длины.
 
